39682ed9624fa4e8c290c7fa083a4d1f63e38413
xiao847 <32233283+xiao847@users.noreply.github.com>
2019-12-06 14:18:24 +0800

    push-images.sh

---
+++ push-images.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for imagepath in $(cat ./imagepath.txt)
+do
+imagename=$(echo $imagepath | awk -F '/' '{print $NF}')
+docker pull $imagepath
+
+# push到阿里云仓库
+docker tag $imagepath registry.cn-hangzhou.aliyuncs.com/aliwill/$imagename
+docker push registry.cn-hangzhou.aliyuncs.com/aliwill/$imagename
