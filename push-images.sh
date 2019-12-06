0aa5898d4b15e5c3b1e75188dce442e9ec5cb277
xiao847 <32233283+xiao847@users.noreply.github.com>
2019-12-06 15:09:48 +0800

    Update push-images.sh

--- push-images.sh
+++ push-images.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+#push google or coreos images to dockerhub or aliyun registry,
+#change the registry name and username/password to yourself's.
 
-for imagepath in $(cat ./imagepath.txt)
+DOCKER_HUB=willdockerhub
+ALI_REGISTRY=registry.cn-hangzhou.aliyuncs.com/aliwill
+
+#config the following env at travis-ui
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker login registry.cn-hangzhou.aliyuncs.com -u $ALI_USERNAME  -p $ALI_PASSWORD
+
+for image in $(cat img-list.txt)
 do
-imagename=$(echo $imagepath | awk -F '/' '{print $NF}')
-docker pull $imagepath
-
-# push到阿里云仓库
-docker tag $imagepath registry.cn-shanghai.aliyuncs.com/k8s_nfyg/$imagename
-docker pushregistry.cn-shanghai.aliyuncs.com/k8s_nfyg/$imagename
+	imagename=$(echo $image | awk -F '/' '{print $NF}')
+	docker pull $image
+	docker tag $image $ALI_REGISTRY/$imagename
+	# push到阿里云仓库
+	docker push $ALI_REGISTRY/$imagename
+done
